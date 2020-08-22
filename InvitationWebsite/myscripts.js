4bc2984a22a1be165fdf01a7748122e186cc28ee
SoanSaini <soansaini@rauland.com.au>
2020-08-22 10:20:02 +1000

    initial commit

---
+++ InvitationWebsite/myscripts.js
@@ -0,0 +1,48 @@
+
+var currentPosition = 1
+var divs = [1, 2, 3, 4]
+var last_element = divs[divs.length - 1];
+var first_element = divs[0];
+
+function nextButtonClicked() {
+	currentPosition = currentPosition + 1
+
+	if(currentPosition === (first_element + 1))
+	{
+		$("#backButton").removeClass('d-none');
+	}
+	else if(currentPosition === last_element)
+	{
+		$("#nextButton").addClass('d-none');
+	}
+	updateDivAtCurrentPosition(currentPosition)
+}
+
+function backButtonClicked() {
+	currentPosition = currentPosition - 1
+
+	if(currentPosition === first_element)
+	{
+		$("#backButton").addClass('d-none');
+	}
+	else if(currentPosition === (last_element - 1))
+	{
+		$("#nextButton").removeClass('d-none');
+	}
+	updateDivAtCurrentPosition(currentPosition)
+}
+
+
+function updateDivAtCurrentPosition(id)
+{
+	divs.forEach(function(entry) {
+		if(entry === id)
+		{
+			$("#"+entry).removeClass('d-none');
+		}
+		else
+		{
+			$("#"+entry).addClass('d-none');
+		}
+	});
+}
